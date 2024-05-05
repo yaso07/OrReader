0fac140b43f97efd942e85dc833a8144456580db
yaso07 <89770165+yaso07@users.noreply.github.com>
2024-05-05 18:50:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react'
 import axios from 'axios'
 import { useState } from 'react';
 import {QrReader} from "react-qr-reader";
-export const QrCodeReader = () => {
+import { toast } from "react-toastify";
+const QrCodeReader = () => {
   const [qrCodeData, setQrCodeData] = useState(null);
   const [error, setError] = useState(null);
 
   const handleScan = async(data) => {
     if (data) {
       console.log("yes")
-       const data = await axios.patch(
-                 `http://localhost:3200/api/seller/currentUser/6634686a9ffcbd2f03c970d8`,
-                 {payment:true}
-               );
+      
       setQrCodeData(data); 
     }
   };
 
+  const handlePayment=async()=>{
+          toast.success("payment done");
+       const data = await axios.patch(
+                 `http://localhost:3200/api/seller/currentUser/6634686a9ffcbd2f03c970d8`,
+                 {payment:true}
+               );
+  }
   const handleError = (err) => {
     console.error(err);
     setError(err);
@@ -27,23 +32,38 @@ export const QrCodeReader = () => {
      facingMode: "environment", // Set to "environment" for back camera
    };
   return (
-    <div>
-      <h1 className="text-center">Scan</h1>
+    <div style={{ width: "100%" }}>
+      <h1 style={{ textAlign: "center" }}>Scan and Pay</h1>
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
         constraints={constraints}
-        style={{ width: "100%" }}
+        style={{ height: "20%", width: "50%" }}
       />
       {error && <p>Error: {error.message}</p>}
       {qrCodeData && (
         <div>
-          <p>Text: {qrCodeData.text}</p>
-          <p>Format: {qrCodeData.format}</p>
+          <p>payment paid</p>
+          <p></p>
           {/* Render other desired properties */}
         </div>
       )}
+      <div style={{ textAlign: "center" }}>
+        <button
+
+          style={{
+            width: "200px",
+            padding: "10px",
+            backgroundColor: "rgb(77, 184, 211)",
+            borderRadius: "10px",
+            border: "1px solid rgb(77, 184, 211)",
+          }}
+          onClick={handlePayment}
+        >
+          Pay
+        </button>
+      </div>
     </div>
   );
 };
@@ -54,7 +74,7 @@ const App = () => {
     <>
     
       <QrCodeReader/>
-     
+      
     </>
   );
 }
